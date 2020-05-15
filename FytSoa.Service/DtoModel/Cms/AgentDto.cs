631479bae70dde35dd17bbb35220a0b1fa6f2b01
helei <757043495@qq.com>
2020-05-15 10:00:26 +0800

    更新代理商模块代码

---
+++ FytSoa.Service/DtoModel/Cms/AgentDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FytSoa.Service.DtoModel.Wx;
+
+namespace FytSoa.Service.DtoModel
+{
+    [Serializable]
+    public class AgentDto
+    {
+        public int Id { get; set; }
+        public string Admin_Guid { get; set; }
+        public string Name { get; set; }
+        public int Level_Id { get; set; }
+        public string Tel { get; set; }
+        public string Business_Area { get; set; }
+        public SettleType Settle_Type { get; set; }
+        public string Account_name { get; set; }
+        public string Account_no { get; set; }
+        public string Account_info { get; set; }
+        public decimal Wxpay { get; set; }
+        public decimal Alipay { get; set; }
+        public decimal Otherpay { get; set; }
+        public DateTime? Create_time { get; set; }
+        public DateTime? Update_time { get; set; }
+        public bool Status { get; set; }
+        public bool Delete { get; set; }
+        public string Level_Name { get; set; }
+    }
+}
