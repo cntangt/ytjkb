b05b28c9dc5e0b9d8425481dfbb28220641fdd72
唐田 <cntang@live.cn>
2020-05-17 15:23:13 +0800

    更新子商户展示

---
+++ FytSoa.Web/Pages/FytAdmin/Cms/MerchantDetail.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using FytSoa.Core.Model.Cms;
+using FytSoa.Service.Interfaces.Cms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace FytSoa.Web.Pages.FytAdmin.Cms
+{
+    public class MerchantDetailModel : PageModel
+    {
+        private readonly ICmsMerchantService merchantService;
+
+        public MerchantDetailModel(ICmsMerchantService merchantService)
+        {
+            this.merchantService = merchantService;
+        }
+
+        [BindProperty]
+        public CmsMerchant Merchant { get; set; }
+
+        public async Task OnGetAsync(int id = 0)
+        {
+            Merchant = (await merchantService.GetModelAsync(m => m.id == id)).data;
+        }
+    }
+}
