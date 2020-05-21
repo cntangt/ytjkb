3e9767e235f980b3ceb74d1b7221ed5fff6ff03b
helei <757043495@qq.com>
2020-05-21 15:17:38 +0800

    添加用户门店授权模块文件

---
+++ FytSoa.Core/Model/Sys/AdminShopRel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using SqlSugar;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FytSoa.Core.Model.Sys
+{
+    [MySugarTable("admin_shop_rel")]
+    public class AdminShopRel
+    {
+        [SugarColumn(IsIdentity = true)]
+        public int id { get; set; }
+        public string admin_guid { get; set; }
+        public string out_shop_id { get; set; }
+        public string out_mch_id { get; set; }
+        public string sub_out_mch_id { get; set; }
+    }
+}
